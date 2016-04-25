4cbc61f54b5d211eb199e2329f03555ae22e8158
bitency <info@bitency.nl>
2016-04-25 10:58:03 +0200

    Create Subscription.php

---
+++ src/Types/Subscription.php
@@ -0,0 +1,20 @@
+<?php
+namespace Hyperized\Wefact\Types;
+use Hyperized\Wefact\WefactAPI;
+/**
+ * Class Subscription
+ * @package Hyperized\Wefact\Types
+ */
+class Subscription extends WefactAPI
+{
+    /**
+     * @var array
+     */
+    protected $allowed = [
+        'add',
+        'edit',
+        'list',
+        'show',
+        'terminate',
+    ];
+}
