8f353ba2108fec60a5a419e02f97e89719ce0380
nickurt <nickurt@users.noreply.github.com>
2017-10-16 14:46:03 +0200

    using env-values to store sensitive information

--- src/config/Wefact.php
+++ src/config/Wefact.php
@@ -4,10 +4,10 @@
 
 return [
     // Full URI to api.php location
-    'api_v2_url' => 'https://yoursite.tld/Pro/apiv2/api.php',
+    'api_v2_url' => env('WEFACT_URL', 'https://yoursite.tld/Pro/apiv2/api.php'),
 
     // Token provided as 'Beveiligingscode'
-    'api_v2_key' => 'token',
+    'api_v2_key' => env('WEFACT_KEY', 'token'),
 
     // Timeout in seconds
     'api_v2_timeout' => 10,
