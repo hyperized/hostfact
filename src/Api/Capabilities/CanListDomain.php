9633e5b54169738f14234de2ec158c8c628e7194
Gerben Geijteman <gerben@hyperized.net>
2021-11-26 19:11:07 +0100

    Add Handle

---
+++ src/Api/Capabilities/CanListDomain.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Hyperized\Hostfact\Api\Capabilities;
+
+trait CanListDomain
+{
+    /**
+     * @param array<string, mixed> $input
+     * @return string
+     */
+    public function listDomain(array $input): string
+    {
+        return $this
+            ->doRequest(
+                self::$name,
+                mb_strtolower(__FUNCTION__),
+                $input
+            );
+    }
+}
---
+++ src/Api/Controllers/Handle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hyperized\Hostfact\Api\Controllers;
+
+use Hyperized\Hostfact\Api\Capabilities\CanAdd;
+use Hyperized\Hostfact\Api\Capabilities\CanDelete;
+use Hyperized\Hostfact\Api\Capabilities\CanEdit;
+use Hyperized\Hostfact\Api\Capabilities\CanList;
+use Hyperized\Hostfact\Api\Capabilities\CanListDomain;
+use Hyperized\Hostfact\Api\Capabilities\CanShow;
+use Hyperized\Hostfact\ApiClient;
+use Hyperized\Hostfact\Interfaces\HandleInterface;
+use Hyperized\Hostfact\Traits\CanCreateNewCustomSelf;
+use Hyperized\Hostfact\Traits\CanCreateNewSelf;
+
+class Handle extends ApiClient implements HandleInterface
+{
+    use CanCreateNewSelf;
+    use CanCreateNewCustomSelf;
+
+    use CanShow;
+    use CanList;
+    use CanAdd;
+    use CanEdit;
+    use CanDelete;
+    use CanListDomain;
+
+    protected static string $name = 'handle';
+}
