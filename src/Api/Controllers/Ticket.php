dc19a7876a0a1875973f1c3d708f988364946e7b
Gerben Geijteman <gerben@hyperized.net>
2021-11-26 19:40:47 +0100

    Add Ticket

---
+++ src/Api/Controllers/Ticket.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Hyperized\Hostfact\Api\Controllers;
+
+use Hyperized\Hostfact\Api\Capabilities\CanAdd;
+use Hyperized\Hostfact\Api\Capabilities\CanAddMessage;
+use Hyperized\Hostfact\Api\Capabilities\CanChangeOwner;
+use Hyperized\Hostfact\Api\Capabilities\CanChangeStatus;
+use Hyperized\Hostfact\Api\Capabilities\CanDelete;
+use Hyperized\Hostfact\Api\Capabilities\CanDownloadAttachment;
+use Hyperized\Hostfact\Api\Capabilities\CanEdit;
+use Hyperized\Hostfact\Api\Capabilities\CanList;
+use Hyperized\Hostfact\Api\Capabilities\CanShow;
+use Hyperized\Hostfact\ApiClient;
+use Hyperized\Hostfact\Interfaces\TicketInterface;
+use Hyperized\Hostfact\Traits\CanCreateNewCustomSelf;
+use Hyperized\Hostfact\Traits\CanCreateNewSelf;
+
+class Ticket extends ApiClient implements TicketInterface
+{
+    use CanCreateNewSelf;
+    use CanCreateNewCustomSelf;
+
+    use CanShow;
+    use CanList;
+    use CanAdd;
+    use CanEdit;
+    use CanDelete;
+    use CanAddMessage;
+    use CanChangeStatus;
+    use CanChangeOwner;
+    use CanDownloadAttachment;
+
+    protected static string $name = 'ticket';
+}
